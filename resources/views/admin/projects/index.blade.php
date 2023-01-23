0c86481ad2223d56cbd3f70b5a21a712b161b2cc
MaTtiaa01 <mattia2001@live.it>
2023-01-23 12:18:00 +0100

    fix delete button

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -37,37 +37,37 @@
                     <td class="d-flex flex-column align-items-center justify-content-center">
                         <a class="btn btn-primary btn-sm" href="{{route('admin.projects.edit',$project->id)}}"><i class="fa-solid fa-pencil"></i></a>
                         <a class="btn btn-primary btn-sm my-2" href="{{route('admin.projects.show',$project->id)}}"><i class="fa-solid fa-eye"></i></a>
-                        <form action="{{route('admin.projects.destroy',$project->id)}}" method="post">
-                            @csrf
-                            @method('DELETE')
 
+                        <!-- Modal trigger button -->
+                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#project-{{$project->id}}">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
 
-                            <!-- Modal trigger button -->
-                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modalId">
-                                <i class="fa-solid fa-trash"></i>
-                            </button>
-
-                            <!-- Modal Body -->
-                            <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                            <div class="modal fade" id="modalId" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="modalTitleId">Delete form</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            Do you want to delete this project permanently?
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-primary">Confirm</button>
-                                        </div>
+                        <!-- Modal Body -->
+                        <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                        <div class="modal fade" id="project-{{$project->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modal-{{$project->id}}">Delete {{$project->title}}</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Do you want to delete this project permanently?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <form action="{{route('admin.projects.destroy',$project->id)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">Confirm</button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
-                        </form>
+
                     </td>
                 </tr>
                 @empty
